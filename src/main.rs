a2970ca4fc23c0d11f9a88e220ba49cf9b1a7381
PSMusicalRoc <timbishop514@gmail.com>
2024-02-26 21:23:29 -0500

    vga buffer working

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,36 @@
 #![no_std]
 #![no_main]
 
+// #![feature(custom_test_frameworks)]
+// #![test_runner(crate::test_runner)]
+
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+use vga_buffer::*;
+
+
+// #[cfg(test)]
+// pub fn test_runner(tests: &[&dyn Fn()]) {
+//     println!("Running {} tests...", tests.len());
+//     for test in tests {
+//         test();
+//     }
+// }
+
+
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // Printing hello world to the screen :)
-    
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    print!("Deez Nuts\n\nGottem");
+    WRITER.lock().write_byte(0x01);
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
